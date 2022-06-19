13c1ba2ceddbd79b0dbf098287e7390097d838a3
Robin Wilson <robin@rtwilson.com>
2022-06-19 19:47:00 +0100

    Clear binary string after processing

--- random_oddeven.py
+++ random_oddeven.py
@@ -27,10 +27,12 @@ try:
 
         if len(binary_string) == 8:
             byte_value = int(binary_string, base=2)
+            binary_string = ""
             bytes_list.append(byte_value)
             print("\n")
             print(f"Byte value = {byte_value}")
             print(f"Current output length = {len(bytes_list)}")
+            
 except KeyboardInterrupt:
     print(f"Writing output to {output_filename}")
     with open(output_filename, 'wb') as f:
