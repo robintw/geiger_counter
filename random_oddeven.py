5d15faf407f9ce3904b29733f4a7d8945a304f68
Robin Wilson <robin@rtwilson.com>
2022-06-19 19:45:40 +0100

    Fix error

--- random_oddeven.py
+++ random_oddeven.py
@@ -28,9 +28,9 @@ try:
         if len(binary_string) == 8:
             byte_value = int(binary_string, base=2)
             bytes_list.append(byte_value)
-        print("\n")
-        print(f"Byte value = {byte_value}")
-        print(f"Current output length = {len(bytes_list)}")
+            print("\n")
+            print(f"Byte value = {byte_value}")
+            print(f"Current output length = {len(bytes_list)}")
 except KeyboardInterrupt:
     print(f"Writing output to {output_filename}")
     with open(output_filename, 'wb') as f:
