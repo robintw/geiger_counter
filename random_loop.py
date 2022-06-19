430134d9c4d5f943262ee311f355dbcbd10b5cf6
Robin Wilson <robin@rtwilson.com>
2022-06-19 21:17:12 +0100

    Add export of bytes to random loop

--- random_loop.py
+++ random_loop.py
@@ -1,16 +1,36 @@
 import RPi.GPIO as GPIO
 import time
+import sys
 
 CHANNEL = 26
 MAX_VALUE = 16
 
-GPIO.setmode(GPIO.BCM)
+output_filename = sys.argv[1]
 
-GPIO.setup(CHANNEL, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(CHANNEL, GPIO.FALLING, bouncetime=100)
+try:
+    GPIO.setmode(GPIO.BCM)
 
-count = 0
-while True:
-    count = (count + 1) % MAX_VALUE
-    if GPIO.event_detected(CHANNEL):
-        print(count)
+    GPIO.setup(CHANNEL, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(CHANNEL, GPIO.FALLING, bouncetime=100)
+
+    count = 0
+    value1 = None
+    value2 = None
+    bytes_list = []
+    while True:
+        count = (count + 1) % MAX_VALUE
+        if GPIO.event_detected(CHANNEL):
+            if value1 is None:
+                value1 = count
+            else:
+                value2 = count
+
+            if value1 is not None and value2 is not None:
+                byte_value = (value1 << 4) | value2
+                print(byte_value)
+                bytes_list.append(byte_value)
+except KeyboardInterrupt:
+    print(f"Writing output to {output_filename}")
+    with open(output_filename, 'wb') as f:
+        f.write(bytes(bytes_list))
+            
