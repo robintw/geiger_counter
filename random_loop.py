af21fd4719519728581593b780bcf496e7ea9509
Robin Wilson <robin@rtwilson.com>
2022-08-16 20:12:25 +0100

    Update to write out every 1024 bytes

--- random_loop.py
+++ random_loop.py
@@ -7,6 +7,15 @@ MAX_VALUE = 16
 
 output_filename = sys.argv[1]
 
+file_id = 1
+
+def write_file(bytes_list):
+    output_filename = output_filename + "_" + str(file_id)
+    file_id += 1
+    print(f"Writing output to {output_filename}")
+    with open(output_filename, 'wb') as f:
+        f.write(bytes(bytes_list))
+
 try:
     GPIO.setmode(GPIO.BCM)
 
@@ -33,8 +42,10 @@ try:
                 bytes_list.append(byte_value)
                 value1 = None
                 value2 = None
+
+                if len(bytes_list) >= 1024:
+                    write_file(bytes_list)
+                    bytes_list = []
 except KeyboardInterrupt:
-    print(f"Writing output to {output_filename}")
-    with open(output_filename, 'wb') as f:
-        f.write(bytes(bytes_list))
+    write_file(bytes_list)
             
